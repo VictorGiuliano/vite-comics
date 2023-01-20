25a1c0339e66fa73166c72c66385a7fa51e7cf23
VictorGiuliano <victorgiuliano98@gmail.com>
2023-01-20 17:43:32 +0100

    Add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,41 @@ export default {
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  min-height: 100vh;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+  padding: 3rem 0.57rem;
+  color: black;
+}
+
+a:hover,
+a:active {
+  color: blue;
+  border-bottom: blue solid 2px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,106 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+
+        }
+
+    }
 }
 </script>
 
 <template>
-
+    <header>
+        <div class="container">
+            <nav class="navbar">
+                <div class="navbar-logo border"><i class="fa-brands fa-discord"></i>
+                    <img src="../assets/img/dc-logo.png">
+                </div>
+                <div class="navbar-menu">
+                    <ul>
+                        <li v-for="link in links" :key="link.text" :class="{ 'current': link.current }"><a
+                                :href="link.url">{{ link.text }}</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
 </template>
-
+    
 <style>
+.container {
+    max-width: 1200px;
+    margin: 0 100px;
+    padding: 0 20px;
+}
 
+.navbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: white;
+    width: 100%;
+}
+
+.navbar-actions {
+    text-align: left;
+}
+
+.navbar-menu {
+
+    text-align: right;
+}
 </style>
